8170290b6267c47cc035c1973904fc20183d9f2a
Martin Murphy <martin.murphy@gmail.com>
2025-05-29 05:39:55 -0500

    Fix issues with crawl and saving notes

--- SavvyCrawler/Crawler.cs
+++ SavvyCrawler/Crawler.cs
@@ -150,45 +150,7 @@ namespace SavvyCrawler
                 Visited.Add(absolutePath);
                 if (!testOnly)
                     counter.Examine(text);
-                if (!isHtml || testOnly) return;
-                var doc = new HtmlDocument();
-                doc.LoadHtml(text);
-                var nodes = doc.DocumentNode.SelectNodes("//a[@href]");
-                if (nodes != null)
-                {
-                    foreach (HtmlNode link in nodes)
-                    {
-                        // Get the value of the HREF attribute
-                        string hrefValue = link.GetAttributeValue("href", string.Empty);
-                        try
-                        {
-                            if (string.IsNullOrEmpty(hrefValue)) continue;
-                            if (!hrefValue.StartsWith("http") && !hrefValue.StartsWith("tel:") && !hrefValue.StartsWith("mobile:"))
-                            {
-                                var hyper = new Uri(absolutePath);
-
-                                var hrefHost = hyper.Host;
-                                if (!hrefValue.StartsWith('/'))
-                                    hrefValue = "/" + hrefValue;
-                                hrefValue = $"{uri.Scheme}://{hrefHost}{hrefValue}";
-                                Links.Add(hrefValue);
-                            }
-                            else
-                            {
-                                var hrefHost = new Uri(hrefValue).Host;
-                                if (!string.IsNullOrEmpty(hrefValue) && hrefHost.Contains(host) && !Links.Contains(hrefValue))
-                                    Links.Add(hrefValue);
-                            }
-
-
-                        }
-                        catch (Exception ex)
-                        {
-                            return;
-                        }
-                    }
-                }
-            }
+        }
         
 
         public async Task<string> ParseHtml(string absolutePath)
